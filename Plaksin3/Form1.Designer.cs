9f314ea14d8b7243561f8d2d047743bc833e73a3
Николай Перминов <kolya-perminov@ya.ru>
2021-05-20 09:47:06 +0500

    Добавьте файлы проекта.

---
+++ Plaksin3/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Plaksin3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(666, 27);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(121, 23);
+            this.add.TabIndex = 0;
+            this.add.Text = "Добавить элемент";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите срок годности";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите имя товара";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(442, 23);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 27);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 71);
+            this.trackBar1.Maximum = 95;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(775, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(151, 53);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(34, 15);
+            this.timeLabel.TabIndex = 8;
+            this.timeLabel.Text = "00:00";
+            this.timeLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Время срока годности:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 122);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(775, 287);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 419);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(209, 19);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "С наибольшим сроком годности";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(635, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Найти элемент";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.add);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(815, 489);
+            this.MinimumSize = new System.Drawing.Size(815, 489);
+            this.Name = "Form1";
+            this.Text = "Посик наиболее старого товара";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Plaksin3/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Plaksin3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<(string Name, DateTime Date, int Time)> mas = new List<(string Name, DateTime Date, int Time)>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mas.Add((
+                Name: textBox1.Text,
+                Date: dateTimePicker1.Value,
+                Time: trackBar1.Value
+                ));
+            richTextBox1.Text +=
+                $"Name: {textBox1.Text}, Date: {dateTimePicker1.Value.ToString("dd.MM.yyyy")}, Time: {((trackBar1.Value * 15) / 60).ToString() + ":" + ((trackBar1.Value * 15) % 60).ToString()}\n";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timeLabel.Text = ((trackBar1.Value * 15) / 60).ToString() + ":" + ((trackBar1.Value * 15) % 60).ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            (string Name, DateTime Date, int Time) o;
+            if (!checkBox1.Checked)
+            {
+                for (int i = 0; i < mas.Count; i++)
+                {
+                    for (int j = 0; j < mas.Count; j++)
+                    {
+                        if (mas[i].Date.Year * 1000000 + mas[i].Date.Month * 10000 + mas[i].Date.Day * 100 +
+                            mas[i].Time < mas[j].Date.Year * 1000000 + mas[j].Date.Month * 10000 +
+                            mas[j].Date.Day * 100 + mas[j].Time)
+                        {
+                            o = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = o;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < mas.Count; i++)
+                {
+                    for (int j = 0; j < mas.Count; j++)
+                    {
+                        if (mas[i].Date.Year * 1000000 + mas[i].Date.Month * 10000 + mas[i].Date.Day * 100 +
+                            mas[i].Time > mas[j].Date.Year * 1000000 + mas[j].Date.Month * 10000 +
+                            mas[j].Date.Day * 100 + mas[j].Time)
+                        {
+                            o = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = o;
+                        }
+                    }
+                }
+            }
+
+            List<(int nachalo, int konec)> t = new List<(int nachalo, int konec)>();
+            richTextBox1.Text = "";
+            for (int i = 0; i < mas.Count; i++)
+            {
+                // ((trackBar1.Value * 15) / 60)
+                if (mas[i].Date.Date > DateTime.Now.Date)
+                {
+                    richTextBox1.Text +=
+                        $"Name: {mas[i].Name}, Date: {mas[i].Date.ToString("dd.MM.yyyy")}, Time: {((mas[i].Time * 15) / 60).ToString() + ":" + ((mas[i].Time * 15) % 60).ToString()}\n";
+                    Debug.Print("1");
+                    continue;
+                }
+                else
+                {
+                    if (mas[i].Date.Date == DateTime.Now.Date)
+                        if (((mas[i].Time * 15) / 60) > DateTime.Now.Hour)
+                        {
+                            richTextBox1.Text +=
+                                $"Name: {mas[i].Name}, Date: {mas[i].Date.ToString("dd.MM.yyyy")}, Time: {((mas[i].Time * 15) / 60).ToString() + ":" + ((mas[i].Time * 15) % 60).ToString()}\n";
+                            Debug.Print("1");
+                            continue;
+                        }
+                        else
+                        {
+                            if (((mas[i].Time * 15) / 60) == DateTime.Now.Hour)
+                                if (((mas[i].Time * 15) % 60) > DateTime.Now.Minute)
+                                {
+                                    richTextBox1.Text +=
+                                        $"Name: {mas[i].Name}, Date: {mas[i].Date.ToString("dd.MM.yyyy")}, Time: {((mas[i].Time * 15) / 60).ToString() + ":" + ((mas[i].Time * 15) % 60).ToString()}\n";
+                                    Debug.Print("1");
+                                    continue;
+                                }
+                        }
+                }
+
+                string text =
+                    $"Name: {mas[i].Name}, Date: {mas[i].Date.ToString("dd.MM.yyyy")}, Time: {((mas[i].Time * 15) / 60).ToString() + ":" + ((mas[i].Time * 15) % 60).ToString()}\n";
+                richTextBox1.Text += text;
+                t.Add((
+                    nachalo: richTextBox1.Text.Length - text.Length,
+                    konec: text.Length
+                    ));
+                Debug.Print(((trackBar1.Value * 15) / 60).ToString() + DateTime.Now.Hour.ToString());
+                Debug.Print(((trackBar1.Value * 15) % 60).ToString() + DateTime.Now.Minute.ToString());
+                Debug.Print("2");
+            }
+
+            foreach ((int nachalo, int konec) tuple in t)
+            {
+                richTextBox1.SelectionStart = tuple.nachalo;
+                richTextBox1.SelectionLength = tuple.konec;
+                richTextBox1.SelectionColor = Color.Red;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
